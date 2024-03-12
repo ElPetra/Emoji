d852ea4bc183e5b244630b94a11dcac88395e7e3
Elpetra <k158vx@yandex.ru>
2024-03-12 18:06:53 +0300

    adaptive almost is done

--- src/script.js
+++ src/script.js
@@ -1,22 +1,25 @@
-import { data as emoji } from "./emoji.js";  //импортируем базу данных
-const inputNode = document.querySelector(".input");  //задаем переменную в js для инпута
-const h1Node = document.querySelector("h1");  //задаем переменную для главного заголовка
-const containerNode = document.querySelector(".container");  //задаем переменную для контейнера
+import { data as emoji } from "./emoji.js"; //импортируем базу данных
+const inputNode = document.querySelector(".input"); //задаем переменную в js для инпута
+const h1Node = document.querySelector("h1"); //задаем переменную для главного заголовка
+const containerNode = document.querySelector(".container"); //задаем переменную для контейнера
 
-h1Node.addEventListener("click", refresh);//создаем событие для главного заголовка при нажатии на него - вызывается функция обновления страницы
-function refresh() {   //создаем функцию для обновления страницы
-  location.reload();   //задаем обновление страницы
+h1Node.addEventListener("click", refresh); //создаем событие для главного заголовка при нажатии на него - вызывается функция обновления страницы
+function refresh() {
+  //создаем функцию для обновления страницы
+  location.reload(); //задаем обновление страницы
 }
 
 renderCard(emoji); //отображаем базу данных
 
-function renderCard(data) {  //функция перебора карточек
-  data.forEach((el) => {     //перебрать весь массив
-    createCard(el);          //в результате перебора вызывать функцию создания карточек
+function renderCard(data) {
+  //функция перебора карточек
+  data.forEach((el) => {
+    //перебрать весь массив
+    createCard(el); //в результате перебора вызывать функцию создания карточек
   });
 }
 
-function createCard (obj) {
+function createCard(obj) {
   //создаем функцию создания карточек
   const containerNode = document.querySelector(".container"); //обращаемся к контейнеру с карточками
   const card = document.createElement("article"); //создаем карточку
@@ -35,12 +38,14 @@ function createCard (obj) {
   textNode.innerHTML = `${obj.filter}`; //указываем, что брать текст из отфильтрованного ключа
   containerNode.append(card); //поместить карточки в конце контейнера
   card.append(h2Node, h3Node, textNode); //поместить в карточку заголовки h2, h3 и текст в конце контейнера поочередно
-};
+}
 
-function filterKey(obj) {  //создаем функцию, чтобы убрать дубли
-  let keywords = obj.keywords.split(" "); //создаем массив из содержимого ключа keywords
-  let filter = keywords.filter((el, i) => keywords.indexOf(el) === i); //?
-  obj.filter = filter.join(" "); //возвращаем массив обратно в строку
+function filterKey(obj) {
+  //создаем функцию, чтобы убрать дубли
+  // let keywords = obj.keywords.split(" "); //создаем массив из содержимого ключа keywords
+  // let filter = keywords.filter((el, i) => keywords.indexOf(el) === i); //?
+  // obj.filter = filter.join(" "); //возвращаем массив обратно в строку
+  obj.filter = [...new Set(obj.keywords.split(" "))].join(" ");
 }
 
 function search(obj, string) {  //создаем функцию поиска с двумя параметрами
@@ -48,18 +53,17 @@ function search(obj, string) {  //создаем функцию поиска с 
   let find = obj.filter(
     (el) => string === el.title.toLowerCase() || el.filter.includes(string) //задаем фильтр на наличие запроса пользователя в ключах title или ?
   );
-  containerNode.innerHTML = ""; //?
+  containerNode.innerHTML = ""; //делаем страницу пустой от всех карточек, чтобы на пустой странице отобразились только отфильтрованные карточки
   return renderCard(find); //возвращаем результат функции renderCard от переменной find
 }
 
-inputNode.onkeypress = function(event) { //создаем событие при нажатии на инпут
-  let button = event.which || event.keyCode//?
-  if (button == 13) {   //клавиша enter
+inputNode.onkeydown = function (event) {
+  //создаем событие при нажатии на какую-то клавишу (onkeypress)
+  if (event.key === "Enter") {
+    //условие: если клавиша Enter, то ...
     search(emoji, inputNode.value);
   }
-}
-
-
+};
 
 
 
